c4d9d82cc6eed9a38db023471a84f5667a866779
yuandecheng <ydc_ebook@163.com>
2015-06-14 22:46:35 +0800

    fist add

---
+++ Assets/Scripts/Steering.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Steering : MonoBehaviour 
+{
+	public float weight = 1;
+
+	public virtual Vector3 Force()
+	{
+		return new Vector3(0,0,0);
+	}
+}
